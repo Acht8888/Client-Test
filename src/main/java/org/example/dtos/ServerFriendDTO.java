b97e382f66b5dd32a2b4dca380934d76391de3b0
Duy <duyhuanhuy@gmail.com>
2025-07-22 11:51:30 +0700

    Update.

--- src/main/java/org/example/dtos/ServerFriendDTO.java
+++ src/main/java/org/example/dtos/ServerFriendDTO.java
@@ -1,18 +1,28 @@
 package org.example.dtos;
 
+import com.potionprotocol.application.friend.enums.FriendStatus;
+import com.potionprotocol.common.utils.BinarySerializer;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.example.enums.FriendStatus;
 
 import java.util.UUID;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ServerFriendDTO {
+    @BinarySerializer.FieldOrder(0)
     private UUID id;
 
+    @BinarySerializer.FieldOrder(1)
     private UUID userId;
+
+    @BinarySerializer.FieldOrder(2)
     private UUID friendId;
 
+    @BinarySerializer.FieldOrder(3)
     private FriendStatus status;
 }
